34468626d158632d00b7e465144bf1503364ea52
Tom <tc1n15@soton.ac.uk>
2022-05-03 19:09:53 +0100

    Rename signals

--- src/hdl/encode.sv
+++ src/hdl/encode.sv
@@ -6,74 +6,102 @@
 
 module encode_6466b #() (
     // TX Interface from MAC
-    input wire txc,
-    input wire txc2, // txc2 = txc / 2 
-    input wire[31:0] txd,
-    input wire[3:0] txctl,
-    output wire tx_pause, 
+    input wire i_txc,
+    input wire i_txc2, // txc/2
+    input wire[31:0] i_txd,
+    input wire[3:0] i_txctl,
+    output wire i_tx_pause, 
 
     // TX Interface out
-    output wire [65:0] data
+    output wire [65:0] o_txd,
+
+    // Rx interface from pcs
+    input wire i_rxc,
+    input wire i_rxc2, //rxc/2
+    input wire [65:0] i_rxd,
+    input wire i_rx_valid,
+
+    //Rx interface out
+    output logic [31:0] o_rxd,
+    output logic [3:0] o_rxctl,
+    output logic o_rx_valid
 );
 
-    // 32 bit input to 64 bit internal
-    wire [63:0] txd64;
-    wire [7:0] txctl64;
-    logic tick = '0;
+    //*********** Transmit **********//
 
-    logic [31:0] r_txd;
-    logic [3:0] r_txctl;
+    // 32-bit input to 64 bit internal
+    wire [63:0] internal_txd;
+    wire [7:0] internal_txctl;
+    logic [31:0] delayed_i_txd;
+    logic [3:0] delayed_i_txctl;
 
-    always @(posedge txc) begin
-        r_txd <= txd;
-        r_txctl <= txctl;
-        tick <= !tick;
+    always @(posedge i_txc) begin
+        delayed_i_txd <= i_txd;
+        delayed_i_txctl <= i_txctl;
     end
 
-    assign txd64 = {r_txd, txd};
-    assign txctl64 = {r_txctl, txctl};
+    assign internal_txd = {delayed_i_txd, i_txd};
+    assign internal_txctl = {delayed_i_txctl, i_txctl};
+
+
+
 
     // Tx encoding
-
-    wire [1:0] tx_header = (txctl64 == '0) ? 2'b01 : 2'b10;
-
-    // check if this is a start frame
+    wire [7:0] tx_type;
+    logic [63:0] enc_tx_data;
+    wire [1:0] tx_header;
     wire [63:0] tx_ctl_mask, tx_ctl_mask_data;
+
+    assign tx_header = (internal_txctl == '0) ? 2'b01 : 2'b10;
 
     // Set non control data to 0 for comparison of codes
     genvar i;
     generate;
         for (i = 0; i < 8; i++) begin
-            assign tx_ctl_mask_data[((i+1)*8) -1:i*8] = txctl64[i] ? txd64[((i+1)*8) -1:i*8] : 8'h00; //todo cleaner indexing
+            assign tx_ctl_mask_data[((i+1)*8) -1:i*8] = internal_txctl[i] ? internal_txd[((i+1)*8) -1:i*8] : 8'h00; //todo cleaner indexing
         end
     endgenerate
 
-    wire [7:0] tx_type;
-    logic [63:0] tx_data;
-
     //Construct tx data (MSB first)
     always_comb begin
-        if(txctl64 == '0) begin
-            tx_data = txd64;
+        if(internal_txctl == '0) begin
+            enc_tx_data = internal_txd;
         end else begin
-            if(txctl64 ==  8'hff && tx_ctl_mask_data == 64'h0707070707070707) tx_data = 64'h1e00000000000000; // Full IDLE
-            else if(txctl64 ==  8'hf8 && tx_ctl_mask_data == 64'h07070707fb000000) tx_data = {40'h3300000000, txd64[23:0]}; // Start 1
-            else if(txctl64 ==  8'h80 && tx_ctl_mask_data == 64'hfb00000000000000) tx_data = {8'h78, txd64[55:0]}; // Start 2
-            else if(txctl64 ==  8'hff && tx_ctl_mask_data == 64'hfd07070707070707) tx_data = 64'h8700000000000000; //T then idle
-            else if(txctl64 ==  8'h7f && tx_ctl_mask_data == 64'h00fd070707070707) tx_data = {txd64[63:56], 56'h87000000000000}; //D, T then idle
-            else if(txctl64 ==  8'h3f && tx_ctl_mask_data == 64'h0000fd0707070707) tx_data = {txd64[63:48], 48'h870000000000}; //D, T then idle
-            else if(txctl64 ==  8'h1f && tx_ctl_mask_data == 64'h000000fd07070707) tx_data = {txd64[63:40], 40'h8700000000}; //D, T then idle
-            else if(txctl64 ==  8'h0f && tx_ctl_mask_data == 64'h00000000fd070707) tx_data = {txd64[63:32], 32'h87000000}; //D, T then idle
-            else if(txctl64 ==  8'h07 && tx_ctl_mask_data == 64'h0000000000fd0707) tx_data = {txd64[63:24], 24'h870000}; //D, T then idle
-            else if(txctl64 ==  8'h03 && tx_ctl_mask_data == 64'h000000000000fd07) tx_data = {txd64[63:16], 16'h8700}; //D, T then idle
-            else if(txctl64 ==  8'h01 && tx_ctl_mask_data == 64'h00000000000000fd) tx_data = {txd64[63: 8],  8'h87}; //D, T then idle
-            else tx_data = 64'h1e1e1e1e1e1e1e1e; //Error
+            if(internal_txctl ==  8'hff && tx_ctl_mask_data == 64'h0707070707070707) enc_tx_data = 64'h1e00000000000000; // Full IDLE
+            else if(internal_txctl ==  8'hf8 && tx_ctl_mask_data == 64'h07070707fb000000) enc_tx_data = {40'h3300000000, internal_txd[23:0]}; // Start 1
+            else if(internal_txctl ==  8'h80 && tx_ctl_mask_data == 64'hfb00000000000000) enc_tx_data = {8'h78, internal_txd[55:0]}; // Start 2
+            else if(internal_txctl ==  8'hff && tx_ctl_mask_data == 64'hfd07070707070707) enc_tx_data = 64'h8700000000000000; //T then idle
+            else if(internal_txctl ==  8'h7f && tx_ctl_mask_data == 64'h00fd070707070707) enc_tx_data = {internal_txd[63:56], 56'h87000000000000}; //D, T then idle
+            else if(internal_txctl ==  8'h3f && tx_ctl_mask_data == 64'h0000fd0707070707) enc_tx_data = {internal_txd[63:48], 48'h870000000000}; //D, T then idle
+            else if(internal_txctl ==  8'h1f && tx_ctl_mask_data == 64'h000000fd07070707) enc_tx_data = {internal_txd[63:40], 40'h8700000000}; //D, T then idle
+            else if(internal_txctl ==  8'h0f && tx_ctl_mask_data == 64'h00000000fd070707) enc_tx_data = {internal_txd[63:32], 32'h87000000}; //D, T then idle
+            else if(internal_txctl ==  8'h07 && tx_ctl_mask_data == 64'h0000000000fd0707) enc_tx_data = {internal_txd[63:24], 24'h870000}; //D, T then idle
+            else if(internal_txctl ==  8'h03 && tx_ctl_mask_data == 64'h000000000000fd07) enc_tx_data = {internal_txd[63:16], 16'h8700}; //D, T then idle
+            else if(internal_txctl ==  8'h01 && tx_ctl_mask_data == 64'h00000000000000fd) enc_tx_data = {internal_txd[63: 8],  8'h87}; //D, T then idle
+            else enc_tx_data = 64'h1e1e1e1e1e1e1e1e; //Error
         end
     end
 
+    assign o_txd = {tx_header, enc_tx_data};
+
+
+
+
+
+
+    
+    // Rx
+
     
 
+    //Rx
+    wire [63:0] rxd64;
+    wire [7:0] rxctl64;
+    logic rx_tick = '0;
 
-
-
+    always @(posedge i_rxc) begin
+        rx_tick <= rx_tick;
+        o_rxd <= (rx_tick) ? rxd64[31:0] : rxd64[63:32];
+        o_rxctl <= (rx_tick) ? rxctl64[31:0] : rxctl64[63:32];
+    end
 endmodule
--- src/tb/encode_tb.sv
+++ src/tb/encode_tb.sv
@@ -3,38 +3,46 @@ import eg_frames::*;
 module encode_tb();
 
 
-logic txc;
-logic txc2;
-logic[31:0] txd;
-logic[3:0] txctl;
-wire tx_pause;
-wire [65:0] data;
+logic i_txc;
+logic i_txc2;
+logic[31:0] i_txd;
+logic[3:0] i_txctl;
+wire i_tx_pause;
+wire [65:0] o_txd;
+
+logic i_rxc;
+logic i_rxc2; //rxc/2
+logic [65:0] i_rxd;
+logic i_rx_valid;
+wire [31:0] o_rxd;
+wire [3:0] o_rxctl;
+wire o_rx_valid;
 
 
 initial begin 
-    txc = '0;
-    txc2 = '0;
-    txd = '0;
-    txctl = '0;
+    i_txc = '0;
+    i_txc2 = '0;
+    i_txd = '0;
+    i_txctl = '0;
 end
 
 
 //always begin #1.58810509ns txc = ~txc; end
-always begin #2ns txc = ~txc; end
-always begin #4ns txc2 = ~txc2; end
+always begin #2ns i_txc = ~i_txc; end
+always begin #4ns i_txc2 = ~i_txc2; end
 
 initial begin
 
     foreach (eg_tx_data[i]) begin
-        txd = eg_tx_data[i];
-        @(posedge txc);
+        i_txd = eg_tx_data[i];
+        @(negedge i_txc);
     end
 end
 
 initial begin
     foreach (eg_tx_ctl[i]) begin
-        txctl = eg_tx_ctl[i];
-        @(posedge txc);
+        i_txctl = eg_tx_ctl[i];
+        @(negedge i_txc);
     end
 end
 

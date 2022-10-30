0706c6cda8a1b5db86f4961f7d3cade24268640e
ttchuk <ttchisholm@gmail.com>
2022-10-30 12:02:47 +0000

    Second crc test vector

--- src/tb/crc/test_crc.py
+++ src/tb/crc/test_crc.py
@@ -38,17 +38,33 @@ class CRC_TB:
 async def crc_test(dut):
     
     tb = CRC_TB(dut)
-    test_vector = [     
-        int("0x00", 16), int("0x10", 16), int("0xA4", 16), int("0x7B", 16), int("0xEA", 16), int("0x80", 16), 
-        int("0x00", 16), int("0x12", 16), int("0x34", 16), int("0x56", 16), int("0x78", 16), int("0x90", 16), 
-        int("0x08", 16), int("0x00", 16), int("0x45", 16), int("0x00", 16), int("0x00", 16), int("0x2E", 16), 
-        int("0xB3", 16), int("0xFE", 16), int("0x00", 16), int("0x00", 16), int("0x80", 16), int("0x11", 16), 
-        int("0x05", 16), int("0x40", 16), int("0xC0", 16), int("0xA8", 16), int("0x00", 16), int("0x2C", 16), 
-        int("0xC0", 16), int("0xA8", 16), int("0x00", 16), int("0x04", 16), int("0x04", 16), int("0x00", 16), 
-        int("0x04", 16), int("0x00", 16), int("0x00", 16), int("0x1A", 16), int("0x2D", 16), int("0xE8", 16), 
-        int("0x00", 16), int("0x01", 16), int("0x02", 16), int("0x03", 16), int("0x04", 16), int("0x05", 16), 
-        int("0x06", 16), int("0x07", 16), int("0x08", 16), int("0x09", 16), int("0x0A", 16), int("0x0B", 16), 
-        int("0x0C", 16), int("0x0D", 16), int("0x0E", 16), int("0x0F", 16), int("0x10", 16), int("0x11", 16)
+    test_vectors = [  
+        [   
+            int("0x00", 16), int("0x10", 16), int("0xA4", 16), int("0x7B", 16), int("0xEA", 16), int("0x80", 16), 
+            int("0x00", 16), int("0x12", 16), int("0x34", 16), int("0x56", 16), int("0x78", 16), int("0x90", 16), 
+            int("0x08", 16), int("0x00", 16), int("0x45", 16), int("0x00", 16), int("0x00", 16), int("0x2E", 16), 
+            int("0xB3", 16), int("0xFE", 16), int("0x00", 16), int("0x00", 16), int("0x80", 16), int("0x11", 16), 
+            int("0x05", 16), int("0x40", 16), int("0xC0", 16), int("0xA8", 16), int("0x00", 16), int("0x2C", 16), 
+            int("0xC0", 16), int("0xA8", 16), int("0x00", 16), int("0x04", 16), int("0x04", 16), int("0x00", 16), 
+            int("0x04", 16), int("0x00", 16), int("0x00", 16), int("0x1A", 16), int("0x2D", 16), int("0xE8", 16), 
+            int("0x00", 16), int("0x01", 16), int("0x02", 16), int("0x03", 16), int("0x04", 16), int("0x05", 16), 
+            int("0x06", 16), int("0x07", 16), int("0x08", 16), int("0x09", 16), int("0x0A", 16), int("0x0B", 16), 
+            int("0x0C", 16), int("0x0D", 16), int("0x0E", 16), int("0x0F", 16), int("0x10", 16), int("0x11", 16)
+        ],
+        [   int("0x08", 16),  int("0x00", 16),  int("0x20", 16),  int("0x77", 16), int("0x05", 16), int("0x38", 16), int("0x0e", 16), int("0x8b", 16),
+            int("0x00", 16),  int("0x00", 16),  int("0x00", 16),  int("0x00", 16), int("0x08", 16), int("0x00", 16), int("0x45", 16), int("0x00", 16),
+            int("0x00", 16),  int("0x28", 16),  int("0x1c", 16),  int("0x66", 16), int("0x00", 16), int("0x00", 16), int("0x1b", 16), int("0x06", 16),
+            int("0x9e", 16),  int("0xd7", 16),  int("0x00", 16),  int("0x00", 16), int("0x59", 16), int("0x4d", 16), int("0x00", 16), int("0x00", 16),
+            int("0x68", 16),  int("0xd1", 16),  int("0x39", 16),  int("0x28", 16), int("0x4a", 16), int("0xeb", 16), int("0x00", 16), int("0x00", 16),
+            int("0x30", 16),  int("0x77", 16),  int("0x00", 16),  int("0x00", 16), int("0x7a", 16), int("0x0c", 16), int("0x50", 16), int("0x12", 16),
+            int("0x1e", 16),  int("0xd2", 16),  int("0x62", 16),  int("0x84", 16), int("0x00", 16), int("0x00", 16), int("0x00", 16), int("0x00", 16),
+            int("0x00", 16),  int("0x00", 16),  int("0x00", 16),  int("0x00", 16)
+        ]
+    ]
+
+    results = [
+        int("0x1b8831b3", 16),
+        int("0x79f7eb93", 16)
     ]
 
     assert (tb.data_width % 8 == 0)
@@ -59,25 +75,29 @@ async def crc_test(dut):
     def chunker(seq, size):
         return (seq[pos:pos + size] for pos in range(0, len(seq), size))
 
+    for tv, res in zip(test_vectors, results):
 
-    await tb.reset()
-    await FallingEdge(tb.dut.i_clk)
-
-    for ivalues in chunker(test_vector, input_width_bytes):
-
-        ivalue = 0
-        ivalid = 0
-        for i, v in enumerate(ivalues):
-            ivalue = ivalue | (v <<  (i * 8))
-            ivalid = ivalid | (1 << i)
-
-        print(f"ivalue: {ivalue:16x} ")
-        print(f"ivalid: {ivalid:16b} ")
-
-        tb.dut.i_data.value = ivalue
-        tb.dut.i_valid.value = ivalid
+        await tb.reset()
         await FallingEdge(tb.dut.i_clk)
 
-    assert tb.dut.o_crc.value.integer == int("0x1b8831b3", 16)
+        for ivalues in chunker(tv, input_width_bytes):
+
+            ivalue = 0
+            ivalid = 0
+            for i, v in enumerate(ivalues):
+                ivalue = ivalue | (v <<  (i * 8))
+                ivalid = ivalid | (1 << i)
+
+            print(f"ivalue: {ivalue:016x} ")
+            print(f"ivalid: {ivalid:08b} ")
+
+            tb.dut.i_data.value = ivalue
+            tb.dut.i_valid.value = ivalid
+            await FallingEdge(tb.dut.i_clk)
+        
+        tb.dut.i_valid.value = 0
+
+        assert tb.dut.o_crc.value.integer == res, \
+        f'CRC result invalid (expected={res:04x}, actual={tb.dut.o_crc.value.integer:04x})'
    
 
